d50179a44ae49b7406ed630969d591a9e0280b45
Timur <timurkashapov15@gmail.com>
2023-10-04 20:38:34 +0300

    initial

---
+++ build-video-player-Desktop-Debug/CMakeFiles/video-player.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/video-player.dir/main.cpp.obj"
+  "CMakeFiles/video-player.dir/main.cpp.obj.d"
+  "CMakeFiles/video-player.dir/mainwindow.cpp.obj"
+  "CMakeFiles/video-player.dir/mainwindow.cpp.obj.d"
+  "CMakeFiles/video-player.dir/video-player_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/video-player.dir/video-player_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/video-player_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/video-player_autogen.dir/ParseCache.txt"
+  "video-player_autogen"
+  "libvideo-player.dll.a"
+  "video-player.exe"
+  "video-player.exe.manifest"
+  "video-player.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/video-player.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-video-player-Desktop-Debug/CMakeFiles/video-player_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/video-player_autogen"
+  "video-player_autogen/include/ui_mainwindow.h"
+  "video-player_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/video-player_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
